53d02f08b5d117db5c9567696cef7e8c125b8e41
wrxx2019 <44341143+wrxx2019@users.noreply.github.com>
2023-04-18 05:09:55 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -15,15 +15,16 @@ export const defaultEnv = {
     title: "",
     saveSession: true,
     APITemperature: 0.6,
-    continuousDialogue: true,
+    continuousDialogue: false,
     APIModel: "gpt-3.5-turbo" as Model
   },
-  CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI Vercel
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供服务，请填入自己的 Key，长期使用请 [自行部署](https://github.com/ourongxing/chatgpt-vercel#%E9%83%A8%E7%BD%B2%E4%B8%80%E4%B8%AA%E4%BD%A0%E8%87%AA%E5%B7%B1%E7%9A%84-chatgpt-%E7%BD%91%E7%AB%99%E5%85%8D%E8%B4%B9)，简单成本低。
-- 点击每条消息前的头像，可以锁定对话，作为角色设定。[查看更多使用技巧](https://github.com/ourongxing/chatgpt-vercel#使用技巧)。
-- 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
+  CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI
+- 要使用本站，请在左下角设置菜单中输入正确的网站密码。要使用本站，请在左下角设置菜单中输入自己OpenAI账户的API Key。
+- 本站基于[开源代码](https://github.com/ourongxing/chatgpt-vercel)搭建，不存在后台收集用户API Key和任何对话记录的功能，请放心使用。
+- [[Shift]] + [[Enter]] 换行。对话框开头输入 [[/]] 或者 [[空格]] 使用Prompt 预设角色。
+- [[↑]] 可编辑最近一次提问。点击网站名称滚动到顶部，点击对话框滚动到底部。
+- 点击每条消息前的头像，可以锁定对话，作为角色设定。
+- 现在支持多个对话，打开对话设置，点击新建对话。在输入框输入 [[/]][[/]] 或[[空格]][[空格]] 可以切换对话，搜索历史消息。
 `,
   CLIENT_MAX_INPUT_TOKENS: {
     "gpt-3.5-turbo": 4 * 1024,
